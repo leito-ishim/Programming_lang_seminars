c50bc26867910149e7702e61a981d75ebfe471c5
Nikita Kozlov <leito-ishim@mail.ru>
2024-01-31 17:09:44 +0500

    Добавил ДЗ по семинару 7

---
+++ homework_sem7_KozlovNE/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 1: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+
+Console.Write("Введите неотрицательное цисло m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите неотрицательное цисло n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int AckermanFunc(int num1, int num2)
+{
+    if (num1 == 0) return num2 + 1;
+    else if (num2 == 0) return AckermanFunc(num1 - 1, 1);
+
+    return AckermanFunc(num1 - 1, AckermanFunc(num1, num2 - 1));
+}
+
+Console.WriteLine(AckermanFunc(m, n));
+
---
+++ homework_sem7_KozlovNE/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 2: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] array = [1, 2, 3, 4, 5];
+
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i],3}");
+    }
+}
+
+void PrintReverseArray(int[] arr, int index)
+{
+    if (index >= arr.Length) return;
+    PrintReverseArray(arr, index+1);
+    Console.Write($"{arr[index],3}");
+}
+
+PrintArray(array);
+Console.Write(" => ");
+PrintReverseArray(array, 0);
+
+
+
